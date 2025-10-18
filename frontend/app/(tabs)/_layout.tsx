341219b0fb2b42b7a05bb5c9b5e288e8e6f1f2f1
techmehedi <ecommehedi@gmail.com>
2025-10-18 19:44:51 -0400

    frontend(tabs): set tab bar background to 50% opacity

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -62,7 +62,7 @@ function TabLayout() {
                   left: 0,
                   right: 0,
                   bottom: 0,
-                  backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
                 },
                 animatedTabBarStyle,
               ]}
